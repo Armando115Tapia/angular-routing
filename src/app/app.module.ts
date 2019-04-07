c17e809368f3f9ebf960f092cc959406431fb06b
Armando Tapia <jusnfds123@hotmail.com>
2019-04-07 11:11:08 -0500

    A simple project with 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompAComponent } from './comp-a/comp-a.component';
+import { CompBComponent } from './comp-b/comp-b.component';
+import { CompCComponent } from './comp-c/comp-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompAComponent,
+    CompBComponent,
+    CompCComponent
   ],
   imports: [
     BrowserModule,
